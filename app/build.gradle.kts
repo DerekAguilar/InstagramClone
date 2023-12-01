cbc6dd7891a037005de7b565072dff09acb2592f
Derek Eduardo Aguilar Reyes <aguilarrde753@gmail.com>
2023-11-30 19:55:49 -0600

    Parte 2 (Tiene problemas con los importes en Gradle)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,11 +32,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
@@ -53,18 +53,23 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.material:material-icons-extended:1.5.4")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
     implementation("com.google.firebase:firebase-auth-ktx:22.3.0")
     implementation("com.google.firebase:firebase-firestore-ktx:24.9.1")
     implementation("com.google.firebase:firebase-storage-ktx:20.3.0")
     implementation("androidx.navigation:navigation-runtime-ktx:2.7.5")
+    implementation("androidx.navigation:navigation-compose:2.7.5")
+    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -72,9 +77,12 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
-    implementation("com.google.dagger:hilt-android:2.44")
+    implementation("com.google.dagger:hilt-android:2.48")
     kapt("com.google.dagger:hilt-android-compiler:2.44")
+    kapt("androidx.hilt:hilt-compiler:1.1.0")
 }
+
+
 
 kapt {
     correctErrorTypes=true
--- app/src/main/java/com/example/instagramclone/MainActivity.kt
+++ app/src/main/java/com/example/instagramclone/MainActivity.kt
@@ -10,7 +10,21 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.instagramclone.presentation.Authentication.AuthenticationViewModel
+import com.example.instagramclone.presentation.Authentication.LoginScreen
+import com.example.instagramclone.presentation.Authentication.SignUpScreen
+import com.example.instagramclone.presentation.Main.FeedsScreen
+import com.example.instagramclone.presentation.Main.ProfileScreen
+import com.example.instagramclone.presentation.Main.SearchScreen
+import com.example.instagramclone.presentation.SplashScreen
 import com.example.instagramclone.ui.theme.InstagramCloneTheme
+import com.example.instagramclone.util.Screens
 import dagger.hilt.android.AndroidEntryPoint
 
 
@@ -22,9 +36,34 @@ class MainActivity : ComponentActivity() {
             InstagramCloneTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colorScheme.background) {
-
+                    val navController=rememberNavController()
+                    val authViewModel:AuthenticationViewModel=hiltViewModel()
+                    InstagramCloneApp(navController,authViewModel)
                 }
             }
+        }
+    }
+}
+@Composable
+fun InstagramCloneApp(navController: NavHostController,authenticationViewModel: AuthenticationViewModel){
+    NavHost(navController=navController,startDestination= Screens.SplashScreen.route){
+        composable(route=Screens.LoginScreen.route){
+            LoginScreen(navController=navController,authenticationViewModel)
+        }
+        composable(route=Screens.SignUpScreen.route){
+            SignUpScreen(navController,authenticationViewModel)
+        }
+        composable(route=Screens.SplashScreen.route){
+            SplashScreen(navController = navController, authViewModel = authenticationViewModel )
+        }
+        composable(Screens.FeedsScreen.route){
+            FeedsScreen(navController=navController)
+        }
+        composable(Screens.ProfileScreen.route){
+            ProfileScreen(navController=navController)
+        }
+        composable(Screens.SearchScreen.route){
+            SearchScreen(navController=navController)
         }
     }
 }
